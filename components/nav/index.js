367873f7f8f16220f28d9b06fb39ab98db22a26c
Abdul Rauf <rauf11.abdul@gmail.com>
2021-05-04 15:49:36 +0500

    fixed formatting

--- components/nav/index.js
+++ components/nav/index.js
@@ -4,30 +4,30 @@ import Link from 'next/link'
 import Hamburger from '../utils/hamburger'
 
 const Nav = () => {
-	const websiteName = 'Open Process Ventures'
+  const websiteName = 'Open Process Ventures'
 
-	return (
-		<Container>
-			<nav className={styles.nav}>
-				<Link href='/'>
-					<div className={styles.title}>
-						<img src='/frame.svg' alt='logo' />
-						<span>{websiteName}</span>
-					</div>
-				</Link>
+  return (
+    <Container>
+      <nav className={styles.nav}>
+        <Link href="/">
+          <div className={styles.title}>
+            <img src="/frame.svg" alt="logo" />
+            <span>{websiteName}</span>
+          </div>
+        </Link>
 
-				<div className={styles.menus}>
-					<Link href='/about'>About</Link>
-					<Link href='/portfolio'>Portfolio</Link>
-					<Link href='/contact'>Start a Conversation</Link>
-				</div>
+        <div className={styles.menus}>
+          <Link href="/about">About</Link>
+          <Link href="/portfolio">Portfolio</Link>
+          <Link href="/contact">Start a Conversation</Link>
+        </div>
 
-				<div className={styles.toggler}>
-					<Hamburger />
-				</div>
-			</nav>
-		</Container>
-	)
+        <div className={styles.toggler}>
+          <Hamburger />
+        </div>
+      </nav>
+    </Container>
+  )
 }
 
 export default Nav
