f7e052f842a32c5444a930beaf1c790dd2e6bd62
hassantayyab <hassandogar9@gmail.com>
2021-05-14 23:13:33 +0500

    updated members

--- components/about/team/members.js
+++ components/about/team/members.js
@@ -18,8 +18,10 @@ export const members = [
     image: '/member-2.svg',
     description: {
       default:
-        '<p>Maz Hussain has a deep-rooted background in technology, having spent over a decade working as a software engineer and leading various teams within investment banking and digital commerce. <br />He discovered blockchain and cryptocurrencies in 2015 and since has built himself up as a renowned figurehead within the space. <br />Over the last five years, he has garnered media attention as a reputable market analyst, become a sought-after advisor and, as an Ethereum certified developer, is a battle tested blockchain visionary with a deep understanding of the space. <br />His knowledge of architectural development coupled with his experience and connections within a network of industry titans makes Maz an asset capable of moving the needle, leading start-ups in an advisory capacity on both business and technical fronts. </p>',
-      hidden: '',
+        '<p>Maz Hussain has a deep-rooted background in technology, having spent over a decade working as a software engineer and leading various teams within investment banking and digital commerce.</p>',
+      hidden: `<p>He discovered blockchain and cryptocurrencies in 2015 and since has built himself up as a renowned figurehead within the space. </p>
+        <p>Over the last five years, he has garnered media attention as a reputable market analyst, become a sought-after advisor and, as an Ethereum certified developer, is a battle tested blockchain visionary with a deep understanding of the space. </p>
+        <p>His knowledge of architectural development coupled with his experience and connections within a network of industry titans makes Maz an asset capable of moving the needle, leading start-ups in an advisory capacity on both business and technical fronts.</p>`,
     },
   },
   {
@@ -29,8 +31,10 @@ export const members = [
     image: '/member-1.svg',
     description: {
       default:
-        '<p>Jacob is Chief Strategy Officer at t3rn, a Web3 Foundation funded project, incubated by the Substrate Builders Program and backed by Polychain.<br />Prior to t3rn, Jacob was a senior team member at Lisk, where he created the Lisk Academy, one of the foremost education platforms in the blockchain space, before going on to form the Lisk Business Development department.<br />Jacob has contributed to books on blockchain, spoken at countless conferences around the world, been interviewed in mainstream media, as well as served as an advisor to London Blockchain Labs.<br />With Jacobs deep rooted knowledge of the space, and experience handling pitches from a number of high-level prospective investors, he offers valuable insight from the clients side - drilling down on what adds true value to nascent crypto projects and subsequently advising, shaping and refining OPV&rsquo;s offerings into a finely-tuned, high level service.</p>',
-      hidden: '',
+        '<p>Jacob is Chief Strategy Officer at t3rn, a Web3 Foundation funded project, incubated by the Substrate Builders Program and backed by Polychain.</p>',
+      hidden: `<p>Prior to t3rn, Jacob was a senior team member at Lisk, where he created the Lisk Academy, one of the foremost education platforms in the blockchain space, before going on to form the Lisk Business Development department.</p>
+      <p>Jacob has contributed to books on blockchain, spoken at countless conferences around the world, been interviewed in mainstream media, as well as served as an advisor to London Blockchain Labs.</p>
+      <p>With Jacobs deep rooted knowledge of the space, and experience handling pitches from a number of high-level prospective investors, he offers valuable insight from the clients side - drilling down on what adds true value to nascent crypto projects and subsequently advising, shaping and refining OPV&rsquo;s offerings into a finely-tuned, high level service.</p>`,
     },
   },
 ]
