25bedac255718321b5726b93bb4e8c7ea5003f3c
Abdul Rauf <rauf11.abdul@gmail.com>
2021-05-15 21:51:21 +0500

    loader page transitions

---
+++ components/utils/pageTransition.js
@@ -0,0 +1,94 @@
+import { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { SwitchTransition, Transition } from 'react-transition-group'
+import { useFixTimeout } from './useFixTimeout'
+
+const DURATION = 500
+const DELAY = 300
+
+function PageTransition({ router, children }) {
+  const overlayRef = useRef()
+  const firstRender = useRef(true)
+
+  useFixTimeout()
+
+  useEffect(() => {
+    // Run on route change only and not on refresh
+    const tl = gsap.timeline()
+    function animate() {
+      tl.to(overlayRef.current, {
+        duration: DURATION / 1000,
+        scaleX: 1.0,
+        transformOrigin: '0 0',
+        ease: 'slow',
+      }).to(overlayRef.current, {
+        duration: DURATION / 1000,
+        scaleX: 0,
+        transformOrigin: '100% 0',
+        ease: 'slow',
+        delay: DELAY / 1000,
+      })
+    }
+
+    if (!firstRender.current) {
+      animate()
+    } else {
+      firstRender.current = false
+    }
+
+    return () => {
+      tl.kill()
+      ScrollTrigger.getAll().forEach((t) => t.kill())
+    }
+  }, [router.route])
+
+  useEffect(() => {
+    firstRender.current = false
+  }, [])
+
+  const getTransitionStyles = {
+    entering: {
+      position: `absolute`,
+      width: '100%',
+      opacity: 1,
+    },
+    entered: {
+      transition: `opacity ${DURATION}ms ease-in-out`,
+      opacity: 1,
+    },
+    exiting: {
+      transition: `opacity ${DURATION}ms ease-in-out`,
+      opacity: 1,
+    },
+  }
+
+  return (
+    <>
+      <div className="transition-overlay" ref={overlayRef} />
+      <div style={{ position: 'relative' }}>
+        <SwitchTransition>
+          <Transition
+            key={router.route}
+            timeout={{
+              enter: DURATION,
+              exit: DURATION,
+            }}
+          >
+            {(status) => (
+              <div
+                style={{
+                  ...getTransitionStyles[status],
+                }}
+              >
+                {children}
+              </div>
+            )}
+          </Transition>
+        </SwitchTransition>
+      </div>
+    </>
+  )
+}
+
+export default PageTransition
--- components/utils/splash/index.js
+++ components/utils/splash/index.js
@@ -1,17 +1,26 @@
+import { CSSTransition } from 'react-transition-group'
 import { useReady } from '../../../context'
 import styles from './splash.module.scss'
 
 const Splash = () => {
   // use the `useReady()` hook to check if the
   //  app is ready so you can remove the loader.
-  const { isReady } = useReady()
+  const { isReady, progress } = useReady()
+  const isLoading = !isReady
   return (
-    !isReady && (
+    <CSSTransition
+      in={isLoading}
+      timeout={200}
+      classNames="fade"
+      enter={false}
+      unmountOnExit
+    >
       <div className={styles.splash}>
+        <div className={styles.progressBar} style={{ width: `${progress}%` }} />
         <img src="/logo.svg" alt="Splash loader" />
         <h3>Open Process Ventures</h3>
       </div>
-    )
+    </CSSTransition>
   )
 }
 
