7c1a0bbd70aea77df0f8c4a7a8b83b08663adab7
hassantayyab <hassandogar9@gmail.com>
2021-05-15 21:32:08 +0500

    Implemented client feedback and added navbar hover effect on resize

--- components/utils/splash/index.js
+++ components/utils/splash/index.js
@@ -2,14 +2,12 @@ import { useReady } from '../../../context'
 import styles from './splash.module.scss'
 
 const Splash = () => {
-  // use the `useReady()` hook to check if the
-  //  app is ready so you can remove the loader.
   const { isReady } = useReady()
   return (
     !isReady && (
       <div className={styles.splash}>
         <img src="/logo.svg" alt="Splash loader" />
-        <h3>Open Process Ventures</h3>
+        <img src="/logo-text.svg" alt="logo" />
       </div>
     )
   )
